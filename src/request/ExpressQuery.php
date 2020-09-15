5414f279ce80b42af2bd1f3a9df0de451db91df4
thinkphp <thinkphp@qq.com>
2020-09-15 15:42:17 +0800

    快递接口简化参数

--- src/request/ExpressQuery.php
+++ src/request/ExpressQuery.php
@@ -5,12 +5,9 @@ namespace think\api\request;
 use think\api\Request;
 
 /**
- * @method $this withTimestamp($value)
- * @method $this withGzip($value)
  * @method $this withCom($value)
  * @method $this withNu($value)
- * @method $this withSenderPhone($value)
- * @method $this withReceiverPhone($value)
+ * @method $this withPhone($value)
  */
 class ExpressQuery extends Request
 {
