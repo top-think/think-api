a3293cf739bb9d885c7b67c642ce96cf5bb6eb53
thinkphp <thinkphp@qq.com>
2020-09-22 10:40:31 +0800

    调整PHP版本依赖

--- src/request/TrashIndex.php
+++ src/request/TrashIndex.php
@@ -5,6 +5,7 @@ namespace think\api\request;
 use think\api\Request;
 
 /**
+ * @method $this withWord($value)
  * @method $this withMode($value)
  * @method $this withNum($value)
  * @method $this withPage($value)
